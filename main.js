3f62327e9ba5b33404aac227f7168cf94e6fff6c
Augusto Romanelli <augustoromanelli12@gmail.com>
2023-10-04 09:44:48 -0300

    subiendo la concha de la lora

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-document.write("la concha de tu hermana")
-console.log("puto")
+alert("la concha de tu hermana");
+
+console.log("puto");
+
 function saludar(nombre) {
-    let nombre = alert("dime tu nombre")
-    document.write("Hola ${nombre}")
+    let nombre = alert(prompt("dime tu nombre"));
+
+    document.write("Hola " + {nombre})
 }
 
 saludar()
